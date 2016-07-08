a9bfb836079e870dc58f9a2a6244abf9680453de
RidgeKim <RidgeKim@users.noreply.github.com>
2016-07-08 09:24:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+import argparse
+import base64
+from PIL import Image
+from PIL import ImageDraw
+from googleapiclient import discovery
+import httplib2
+from oauth2client.client import GoogleCredentials
+
+
+DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+
+
+def get_vision_service():
+    credentials = GoogleCredentials.get_application_default()
+    return discovery.build('vision', 'v1', credentials=credentials,
+                           discoveryServiceUrl=DISCOVERY_URL)
+
+
+def detect_face(face_file, max_results=10):
+    image_content = face_file.read()
+    batch_request = [{
+        'image': {
+            'content': base64.b64encode(image_content).decode('UTF-8')
+            },
+        'features': [{
+            'type': 'FACE_DETECTION',
+            'maxResults': max_results,
+            }]
+        }]
+
+    service = get_vision_service()
+    request = service.images().annotate(body={
+        'requests': batch_request,
+        })
+    response = request.execute()
+
+    return response['responses'][0]['faceAnnotations']
+
+
+def highlight_faces(image, faces, output_filename):
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    for face in faces:
+        box = [(v.get('x', 0.0), v.get('y', 0.0)) for v in face['fdBoundingPoly']['vertices']]
+        draw.line(box + [box[0]], width=5, fill='#00ff00')
+
+    del draw
+    im.save(output_filename)
+
+
+def main(input_filename, output_filename, max_results):
+    with open(input_filename, 'rb') as image:
+        faces = detect_face(image, max_results)
+        print('Found %s face%s' % (len(faces), '' if len(faces) == 1 else 's'))
+
+        print('Writing to file %s' % output_filename)
+        # Reset the file pointer, so we can read the file again
+        image.seek(0)
+        highlight_faces(image, faces, output_filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Detects faces in the given image.')
+    parser.add_argument(
+        'input_image', help='the image you\'d like to detect faces in.')
+    parser.add_argument(
+        '--out', dest='output', default='out.jpg',
+        help='the name of the output file.')
+    parser.add_argument(
+        '--max-results', dest='max_results', default=4,
+        help='the max results of face detection.')
+    args = parser.parse_args()
+
+    main(args.input_image, args.output, args.max_results)
